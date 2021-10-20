879ebb4aa07ee7a7a434902408b4fecb9560bec3
SkytheFolf <74611387+SkytheFolf@users.noreply.github.com>
2021-10-20 18:37:42 -0400

    Delete install-arm.sh

--- install-arm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo mkdir -p /opt/rainpi
-sudo chmod 777 /opt/rainpi
-wget https://assets.checkra.in/downloads/linux/cli/arm/f060f8e27005b553de42b04abd0af6b18eeacd005e68b126e991c6096f51b0b7/checkra1n -O /opt/rainpi/checkra1n
-sudo chmod +x /opt/rainpi/checkra1n
-sudo cp 01-rainpi.rules /etc/udev/rules.d
-echo "rainpi is now installed!"
